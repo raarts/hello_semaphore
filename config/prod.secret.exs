e5014e621700c5642e0a6f83d8f3585989d94301
Cody Boggs <cjboggs@acm.org>
2016-06-11 22:24:15 -0600

    commit config/prod.secret.exs for the sake of the tutorial. DON'T EVER DO THIS.

---
+++ config/prod.secret.exs
@@ -0,0 +1,15 @@
+use Mix.Config
+
+# In this file, we keep production configuration that
+# you likely want to automate and keep it away from
+# your version control system.
+config :hello_semaphore, HelloSemaphore.Endpoint,
+  secret_key_base: "gFVfYqPW2qtC0xaY81aB6kMt1o5G7O3ehhg9t0JoRiZPMI0Yzhzj0burL3wVqtmw"
+
+# Configure your database
+config :hello_semaphore, HelloSemaphore.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: "postgres",
+  password: "postgres",
+  database: "hello_semaphore_prod",
+  pool_size: 20
